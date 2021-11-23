d74faa43ac3cd702229c2a4c4466cd59dc6e2ad3
Dmitry Shlagoff <richyannet@yandex.ru>
2021-11-23 23:27:09 +0700

    Fix client

--- web3/locker/index.js
+++ web3/locker/index.js
@@ -26,19 +26,43 @@ class Client {
     this.program = initProgram(this.provider, this.cluster, programName);
   }
   async findMintInfoAddress(mint) {
-    return await findMintInfoAddress(this.program, mint);
+    const [mintInfo, bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        mint.toBytes()
+      ],
+      this.program.programId
+    );
+    return [mintInfo, bump];
   }
 
   async findConfigAddress() {
-    return await findConfigAddress(this.program);
+    const [config, bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        new TextEncoder().encode("config")
+      ],
+      this.program.programId
+    );
+    return [config, bump];
   }
 
   async vaultAuthorityAddress(locker) {
-    return await vaultAuthorityAddress(this.provider, locker, this.cluster);
+    return await anchor.web3.PublicKey.createProgramAddress(
+      [
+        locker.publicKey.toBytes(),
+        [locker.account.vaultBump]
+      ],
+      this.program.programId
+    );
   }
 
   async isMintWhitelisted(mint) {
-    return await isMintWhitelisted(this.provider, mint, this.cluster);
+    const [mintInfo, _bump] = await this.findMintInfoAddress(mint);
+
+    return await tryIfExists(
+      this.program, "mintInfo", mintInfo,
+      (mintInfoAccount) => mintInfoAccount.feePaid,
+      () => false,
+    );
   }
 
   async isTokenAccepted(mint) {
@@ -56,7 +80,82 @@ class Client {
   }
 
   async createLocker(args) {
-    return await createLocker(this.provider, args, this.cluster);
+    const [locker, lockerBump] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        args.creator.toBytes(),
+        args.unlockDate.toArray('be', 8),
+        args.amount.toArray('be', 8)
+      ],
+      this.program.programId
+    );
+
+    const [vaultAuthority, vaultBump] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        locker.toBytes()
+      ],
+      this.program.programId,
+    );
+
+    const fundingWalletAccount = await serumCmn.getTokenAccount(this.provider, args.fundingWallet);
+    const vault = new anchor.web3.Account();
+    const createTokenAccountInstrs = await serumCmn.createTokenAccountInstrs(
+      this.provider,
+      vault.publicKey,
+      fundingWalletAccount.mint,
+      vaultAuthority
+    );
+
+    const [config, _] = await this.findConfigAddress();
+    const configAccount = await this.program.account.config.fetch(config);
+
+    const [mintInfo, initMintInfoInstrs] = await getOrCreateMintInfo(
+      this.program,
+      fundingWalletAccount.mint,
+      args.creator,
+      config
+    );
+    const [feeTokenWallet, createAssociatedTokenAccountInstrs] = await utils.getOrCreateAssociatedTokenAccountInstrs(
+      this.provider, fundingWalletAccount.mint, configAccount.feeWallet
+    );
+
+    const finalFeeWallet = args.feeInSol ? configAccount.feeWallet : feeTokenWallet;
+
+    await this.program.rpc.createLocker(
+      {
+        unlockDate: args.unlockDate,
+        lockerBump,
+        vaultBump,
+        countryCode: args.countryCode,
+        startEmission: args.startEmission,
+        amount: args.amount,
+        feeInSol: args.feeInSol,
+      },
+      {
+        accounts: {
+          locker,
+          creator: args.creator,
+          owner: args.owner,
+          vault: vault.publicKey,
+          vaultAuthority,
+          fundingWalletAuthority: args.fundingWalletAuthority,
+          fundingWallet: args.fundingWallet,
+          feeWallet: finalFeeWallet,
+          mintInfo,
+          countryBanlist: configAccount.countryList,
+          config,
+
+          clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          tokenProgram: utils.TOKEN_PROGRAM_ID,
+        },
+        instructions: createTokenAccountInstrs
+          .concat(initMintInfoInstrs)
+          .concat(createAssociatedTokenAccountInstrs),
+        signers: [vault],
+      }
+    );
+
+    return locker;
   }
 
   async getLockers() {
@@ -79,27 +178,196 @@ class Client {
   }
 
   async relock(args) {
-    return await relock(this.provider, args, this.cluster);
+    return await this.program.rpc.relock(
+      args.unlockDate,
+      {
+        accounts: {
+          locker: args.locker.publicKey,
+          owner: args.locker.account.owner,
+        }
+      }
+    );
   }
 
   async transferOwnership(args) {
-    return await transferOwnership(this.provider, args, this.cluster);
+    const rpcArgs = {
+      accounts: {
+        locker: args.locker.publicKey,
+        owner: args.locker.account.owner,
+        newOwner: args.newOwner,
+      }
+    };
+
+    if (args.signers !== undefined) {
+      rpcArgs.signers = args.signers;
+    }
+
+    return await this.program.rpc.transferOwnership(rpcArgs);
   }
 
   async incrementLock(args) {
-    return await incrementLock(this.provider, args, this.cluster);
+    const [config, _] = await this.findConfigAddress();
+    const configAccount = await this.program.account.config.fetch(config);
+
+    const fundingWalletAccount = await serumCmn.getTokenAccount(this.provider, args.fundingWallet);
+    const [mintInfo, initMintInfoInstrs] = await getOrCreateMintInfo(
+      this.program,
+      fundingWalletAccount.mint,
+      args.fundingWalletAuthority
+    );
+    const [feeTokenWallet, createAssociatedTokenAccountInstrs] = await utils.getOrCreateAssociatedTokenAccountInstrs(
+      this.provider, fundingWalletAccount.mint, configAccount.feeWallet
+    );
+
+    await this.program.rpc.incrementLock(
+      args.amount,
+      {
+        accounts: {
+          locker: args.locker.publicKey,
+          vault: args.locker.account.vault,
+          fundingWallet: args.fundingWallet,
+          fundingWalletAuthority: args.fundingWalletAuthority,
+          feeWallet: feeTokenWallet,
+          tokenProgram: utils.TOKEN_PROGRAM_ID,
+          mintInfo,
+          config
+        },
+        instructions: initMintInfoInstrs
+          .concat(createAssociatedTokenAccountInstrs)
+      }
+    );
+
+    return feeTokenWallet;
   }
 
   async withdrawFunds(args) {
-    return await withdrawFunds(this.provider, args, this.cluster);
+    const vaultAuthority = await anchor.web3.PublicKey.createProgramAddress(
+      [
+        args.locker.publicKey.toBytes(),
+        [args.locker.account.vaultBump]
+      ],
+      this.program.programId,
+    );
+
+    let targetWallet = args.targetWallet;
+    let extraInstructions = [];
+
+    if (args.createAssociated) {
+      const vaultWalletAccount = await serumCmn.getTokenAccount(this.provider, args.locker.account.vault);
+      const [targetTokenWallet, createAssociatedTokenAccountInstrs] = await utils.getOrCreateAssociatedTokenAccountInstrs(
+        this.provider, vaultWalletAccount.mint, targetWallet
+      );
+      targetWallet = targetTokenWallet;
+      extraInstructions.concat(createAssociatedTokenAccountInstrs);
+    }
+
+    await this.program.rpc.withdrawFunds(
+      args.amount,
+      {
+        accounts: {
+          locker: args.locker.publicKey,
+          owner: args.locker.account.owner,
+          vaultAuthority,
+          vault: args.locker.account.vault,
+          targetWallet,
+
+          clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
+          tokenProgram: utils.TOKEN_PROGRAM_ID,
+        },
+        instructions: extraInstructions
+      }
+    );
+
+    return targetWallet;
   }
 
   async closeLocker(args) {
-    return await closeLocker(this.provider, args, this.cluster);
+    const vaultAuthority = await anchor.web3.PublicKey.createProgramAddress(
+      [
+        args.locker.publicKey.toBytes(),
+        [args.locker.account.vaultBump]
+      ],
+      this.program.programId,
+    );
+
+    await this.program.rpc.withdrawFunds(
+      {
+        accounts: {
+          locker: args.locker.publicKey,
+          owner: args.locker.account.owner,
+          vaultAuthority,
+          vault: args.locker.account.vault,
+          targetWallet: args.targetWallet,
+
+          tokenProgram: utils.TOKEN_PROGRAM_ID,
+        }
+      }
+    );
+
+    return vaultAuthority;
   }
 
   async splitLocker(args) {
-    return await splitLocker(this.provider, args, this.cluster);
+    const oldVaultAuthority = await anchor.web3.PublicKey.createProgramAddress(
+      [
+        args.locker.publicKey.toBytes(),
+        [args.locker.account.vaultBump]
+      ],
+      this.program.programId,
+    );
+
+    const [newLocker, newLockerBump] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        args.locker.publicKey.toBytes(),
+        args.locker.account.currentUnlockDate.toArray('be', 8),
+        args.amount.toArray('be', 8),
+      ],
+      this.program.programId
+    );
+
+    const [newVaultAuthority, newVaultBump] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        newLocker.toBytes(),
+      ],
+      this.program.programId,
+    );
+
+    const vaultAccount = await serumCmn.getTokenAccount(this.provider, args.locker.account.vault);
+    const newVault = new anchor.web3.Account();
+    const createTokenAccountInstrs = await serumCmn.createTokenAccountInstrs(
+      this.provider,
+      newVault.publicKey,
+      vaultAccount.mint,
+      newVaultAuthority
+    );
+
+    await this.program.rpc.splitLocker(
+      {
+        amount: args.amount,
+        lockerBump: newLockerBump,
+        vaultBump: newVaultBump,
+      },
+      {
+        accounts: {
+          oldLocker: args.locker.publicKey,
+          oldOwner: args.locker.account.owner,
+          oldVaultAuthority,
+          oldVault: args.locker.account.vault,
+
+          newLocker,
+          newOwner: args.newOwner,
+          newVaultAuthority,
+          newVault: newVault.publicKey,
+
+          systemProgram: anchor.web3.SystemProgram.programId,
+          tokenProgram: utils.TOKEN_PROGRAM_ID,
+        },
+        instructions: createTokenAccountInstrs,
+        signers: [newVault],
+      }
+    );
+
+    return newVault;
   }
 }
 
